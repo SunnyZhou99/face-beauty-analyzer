92cae390f2c99c0c31f8b4ff3424974c25c228f0
SunnyZhou99 <1684430783@qq.com>
2026-02-18 20:44:44 +0800

    new

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // Cloudflare Pages compatibility
-  webpack: (config) => {
-    config.resolve.fallback = { ...config.resolve.fallback, net: false, tls: false, fs: false };
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
